6b44adc853008221f6f54f7657dc410253b9d577
HuubMons <github@huubmons.nl>
2023-05-12 11:06:27 +0200

    Delete lijst_namen.php
    Backwards compatibililty.

--- lijst_namen.php
+++
@@ -1,9 +0,0 @@
-<!-- TODO: @Devs: is it still used? -->
-<head>
-<!-- BACKWARDS COMPATIBLY ONLY!!!! -->
-<script language="JavaScript">
-<!--
-window.location="list_names";
-//-->
-</script>
-</head> 
