929f4fe347644b9f229408ff13941adf666a83eb
HuubMons <github@huubmons.nl>
2023-05-12 11:33:38 +0200

    Revert "Delete lijst.php"
    This reverts commit 41e94ca2724e2ca8a1a90b1375e49cca78065779.

---
+++ lijst.php
@@ -0,0 +1,9 @@
+<!-- TODO: @Devs: is it still used? -->
+<head>
+<!-- BACKWARDS COMPATIBLY ONLY!!!! -->
+<script language="JavaScript">
+<!--
+window.location="list.php";
+//-->
+</script>
+</head> 
