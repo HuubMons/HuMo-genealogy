d45d489ab5179ce082409083492a9e5a4524106e
david-baron <davidbaron@mycodingsoft.com>
2023-05-12 13:29:50 +0200

    French langage utf8

--- languages/language.php
+++ languages/language.php
@@ -23,4 +23,3 @@ include_once(CMS_ROOTPATH."languages/gettext.php");
 $_SESSION["language_selected"]=$selected_language;
 Load_default_textdomain();
 //Load_textdomain('customer_domain', 'languages/'.$selected_language.'/'.$selected_language.'.mo');
-?>
