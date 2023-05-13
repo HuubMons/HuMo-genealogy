9f186ab5419181377aca414f32c3aafbd554e956
david-baron <davidbaron@mycodingsoft.com>
2023-05-13 17:14:13 +0200

    Prepare v7.0

--- externals/areyousure/spec/javascripts/jquery.are-you-sure_spec.js
+++ externals/areyousure/spec/javascripts/jquery.are-you-sure_spec.js
@@ -3,23 +3,23 @@
 // Karma adds 'base/' to the default path
 jasmine.getFixtures().fixturesPath = 'base/spec/javascripts/fixtures';
 
-describe("A form's", function() {
+describe("A form's", function () {
   var $form = undefined;
 
-  describe('text input', function() {
+  describe('text input', function () {
     var $textInput = undefined;
 
-    beforeEach(function() {
+    beforeEach(function () {
       loadFixtures('input-text.html');
       $form = $('form');
       $textInput = $('input[type=text]');
       $form.areYouSure();
     });
 
-    it('should cause dirtyness after its value changes', function(done) {
+    it('should cause dirtyness after its value changes', function (done) {
       expect($form.hasClass('dirty')).toBe(false);
       $textInput.val('new').change();
-      setTimeout(function() {
+      setTimeout(function () {
         expect($form.hasClass('dirty')).toBe(true);
         done();
       }, 0);
