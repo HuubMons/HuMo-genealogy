11b4a982e8ec8c89dde42d18104cb8549a30f4c9
HuubMons <github@huubmons.nl>
2025-01-18 09:46:59 +0100

    Refactor relationship calculator

--- app/controller/relationsController.php
+++ app/controller/relationsController.php
@@ -1,31 +1,40 @@
 <?php
 class RelationsController
 {
-    public function getRelations($db_functions, $person_cls)
+    public function getRelations($db_functions, $person_cls, $link_cls, $uri_path, $tree_id, $selected_language)
     {
-        $RelationsModel = new RelationsModel();
+        $RelationsModel = new RelationsModel($db_functions, $selected_language);
 
         $RelationsModel->resetValues();
         $RelationsModel->checkInput();
+        $RelationsModel->getSelectedPersons($person_cls);
+        $RelationsModel->getNames();
+        $RelationsModel->getGEDCOMnumbers();
+        $RelationsModel->switchPersons();
 
-        $get_persons = $RelationsModel->getSelectedPersons($db_functions, $person_cls);
-        $get_names = $RelationsModel->getNames();
-        $get_GEDCOMnumbers = $RelationsModel->getGEDCOMnumbers();
+        $RelationsModel->set_control_variables();
 
-        //$relation = array(
-        //    "title" => __('Relationship calculator')
-        //);
-
-        // *** Add array $person_data:
-        //$relation = array_merge($relation, $get_persons);
-        $relation = $get_persons;
-        $relation = array_merge($relation, $get_names);
-        $relation = array_merge($relation, $get_GEDCOMnumbers);
-
-        $switch_persons = $RelationsModel->switchPersons($relation);
-        if (isset($switch_persons)) {
-            $relation = array_merge($relation, $switch_persons);
+        // *** Extended search ***
+        if (isset($_POST["extended"]) || isset($_POST["next_path"])) {
+            // TODO check variable. Is session needed?
+            if (!isset($_POST["next_path"])) {
+                $_SESSION['next_path'] = "";
+            }
         }
+
+        // *** Process standard calculation ***
+        $RelationsModel->process_standard_calculation($tree_id, $link_cls, $uri_path);
+
+        if (isset($_POST["extended"]) || isset($_POST["next_path"])) {
+            $RelationsModel->process_extended_calculation();
+        }
+
+
+        $relation = $RelationsModel->get_variables();
+
+        // http://localhost/HuMo-genealogy/family/3/F116?main_person=I202
+        $relation['fam_path'] = $link_cls->get_link($uri_path, 'family', $tree_id, true);
+        $relation['rel_path'] = $link_cls->get_link($uri_path, 'relations', $tree_id);
 
         return $relation;
     }
